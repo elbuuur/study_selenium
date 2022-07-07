92a9fbbde6e8191c4674cd11979af9ea2f74abf4
Ilya <elbuuur@gmail.com>
2022-07-07 23:46:55 +0500

    любим прокси

--- chromedriver/main.py
+++ chromedriver/main.py
@@ -1,4 +1,5 @@
-from selenium import webdriver
+# from selenium import webdriver
+from seleniumwire import webdriver
 from selenium.webdriver.chrome.service import Service
 from fake_useragent import UserAgent
 import time
@@ -10,8 +11,11 @@ user_agent = UserAgent()
 options = webdriver.ChromeOptions()
 options.add_argument(f'user-agent={user_agent.random}')
 
+# set proxy
+options.add_argument('--proxy-server=91.132.151.232:80')
+
 # url = 'https://www.avito.ru/ufa?q=%D0%B4%D0%B8%D0%B2%D0%B0%D0%BD%D1%8B'
-url = 'https://www.whatismybrowser.com/detect/what-is-my-user-agent/'
+url = 'https://2ip.ru/'
 path = Service(r'C:\Users\elbuu\PycharmProjects\selenium\chromedriver\chromedriver.exe')
 
 driver = webdriver.Chrome(
